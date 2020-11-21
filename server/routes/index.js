439caee7b4c031ebcc234b249e0eaf54f4c7075b
HectorSanSmart <hectorsanchezhdn@gmail.com>
2020-11-20 22:57:29 -0600

    Productos y categorias agregadas

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.use(require('./usuario')); //importando y usando las rutas del usuario
 app.use(require('./login')); //importando y usando las rutas del login
-
+app.use(require('./categoria'));
+app.use(require('./producto'));
 //exportando los modulos
 module.exports = app;
